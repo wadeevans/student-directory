f3eee35b11ad2427d41b139b6c3fb67ef383ad7c
wevans <wevans@cix.co.uk>
2013-12-12 14:54:21 +0000

    Added comments to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "----------------------"
 puts "Dr. No"
@@ -5,7 +6,8 @@ puts "Caligula"
 puts "Michael P. Jagger"
 puts "William De Ath"
 puts "Jiminy Cricket"
-
+#Then, we print the total number
 print "Overall, we have "
 print 5
+# print() doesn't automatically add newline like put(). Use \n if you need one
 puts " great students"
