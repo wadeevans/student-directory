3f0f010bbabcd9bdd199ad17492b429c37bf8f17
wevans <wevans@cix.co.uk>
2013-12-12 14:59:04 +0000

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+# variable for student count
+student_count = 5
 # First, we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "----------------------"
@@ -7,7 +9,4 @@ puts "Michael P. Jagger"
 puts "William De Ath"
 puts "Jiminy Cricket"
 #Then, we print the total number
-print "Overall, we have "
-print 5
-# print() doesn't automatically add newline like put(). Use \n if you need one
-puts " great students"
+puts "Overall, we have #{student_count} great students"
