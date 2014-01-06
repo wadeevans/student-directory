782a26c1507adc0eed4bb586c087e78f37556f5a
Chris Wade-Evans <wevans@cix.co.uk>
2014-01-06 16:18:53 +0000

    section 2 menu added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,33 @@
 #start of project section 2
 
+# Interactive_menu
+def interactive_menu
+	students = []
+	loop do
+		# 1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		# 2. read the input and save it into a variable
+		selection = gets.chomp
+		# 3. do what has been asked
+		case selection
+		when "1"
+			# input the students
+			students = input_students
+		when "2"
+			# show the students
+			print_header
+			print_students(students)
+			print_footer(students)
+		when "9"
+			# exit
+		else
+			puts "I don't understand, please try again"
+		end
+	end
+end
+
 # method to add students add attributes
 def input_students
 	printf "Please enter the student names\n"
@@ -109,16 +137,18 @@ def print_footer(students)
 	end
 end
 
+
+interactive_menu()
 # nothing happens until we call the methods
 
-students = input_students()
-print_header
-print_students(students)
+# students = input_students()
+# print_header
+# print_students(students)
 # uncomment the lines below to test other methods
 # print_students_beginning_with_A(students)
 # print_students_with_less_than_12_chars(students)
 # print_students_using_while(students)
 # print_students_using_center(students)
 # print_students_in_cohorts(students)
-print_footer(students)
+# print_footer(students)
 
